66355a9f0810e1753de9caefb16d2d37439ac1a7
John Reiley <supermormonbrother@gmail.com>
2020-01-18 17:49:48 -0700

    week03 assignment

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
+  contact: Contact = null;
+  // {
+  //   id: '3',
+  //   name: 'Superman',
+  //   email: 'kentc@byui.edu',
+  //   phone: '999-999-9999',
+  //   imageUrl: 'https://imgix-media.wbdndc.net/cms/filer_public/56/ec/56ec1125-438b-48f5-ac54-26b7953c7d5a/superman_profile_1.jpg',
+  //   group: null
+  // }
 
   constructor() { }
 
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
-export class ContactListComponent implements OnInit {
+export class ContactListComponent implements OnInit { 
+  contacts: Contact[] = [
+    new Contact(
+      '1', 
+      'Bro. Huntington', 
+      'bh@byui.edu', 
+      '209-345-1234', 
+      'http://www.minneapolisheadshots.com/gallery/main/professionalbusinessheadshot.jpg', 
+      null,
+      ),
+    new Contact(
+      "2",
+      "Bro. Barzee",
+      "barzeer@byui.edu",
+      "208-496-3768",
+      "https://web.byui.edu/Directory/Employee/barzeer.jpg",
+      null
+    )
+  ]
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
